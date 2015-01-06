aef1a34d0f80bb62887c1cbc03a6ce48f5047e7a
rihababdelhedi <rihab.abde@gmail.com>
2015-01-06 15:14:18 +0100

    Added the Command paattern implementation.

---
+++ SharpSimulator/Simulator/Command/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SharpSimulator
+{
+	public class Command
+	{
+		String name;
+		public Command(String name)
+		{
+			this.name=name;
+		}
+		// Move 
+		public void move ()
+		{
+			Logger.LogChain.Message(name+"Command move",Logger.Level.SIMULATION_DEBUG);
+		}
+		public void back ()
+		{
+			Logger.LogChain.Message(name+"Command back",Logger.Level.SIMULATION_DEBUG);
+		}
+	}
+}
+
---
+++ SharpSimulator/Simulator/Command/CommandNull.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SharpSimulator
+{
+	internal class CommandNull:AbstractCommand
+	{
+		public override void execute()
+		{
+			throw new NotImplementedException();
+		}
+		public override void undo()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+}
+
